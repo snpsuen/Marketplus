3e759d5f609bee7e9ae33e97996341c5af116934
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2023-10-24 13:20:10 +0100

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -4,8 +4,8 @@ require('babel-polyfill');
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
+      host: "0.0.0.0",
+      port: 8545,
       network_id: "*" // Match any network id
     },
   },
@@ -13,7 +13,6 @@ module.exports = {
   contracts_build_directory: './src/abis/',
   compilers: {
     solc: {
-	  // version: "0.8.0"
       optimizer: {
         enabled: true,
         runs: 200
